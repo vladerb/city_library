5a47cd8c4ed22d111ce7f7dda557239f1b54eaeb
Vlad Bukhta <bukhta05@gamail.com>
2024-09-21 21:00:24 +0300

    Створення моделі користувача, та його книг

--- library/accounts/models.py
+++ library/accounts/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from ..archive.models import Book
+
+User = get_user_model()
+
+class Profile(models.Model):
+    FORM_OF_STUDY = [
+        ('FT', 'Full-time'),
+        ('PT', 'Part-time'),
+        ('DU', 'Dual'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', default='users/default_autor.webp', blank=True, null=True)
+    place_of_study = models.CharField(max_length=255, blank=True)
+    form_of_study = models.CharField(max_length=2, choices=FORM_OF_STUDY)
+    
+    books_received = models.ManyToManyField(Book, through='BookReceipt', blank=True)
+
+    def __str__(self) -> str:
+        return f'Profile of {self.user.username}'
+
+
+class BookReceipt(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date_received = models.DateField()
+    date_due = models.DateField()
+
+    class Meta:
+        unique_together = ('profile', 'book')
+
+    def __str__(self):
+        return f'{self.book.title} received by {self.profile.user.username}'
--- library/archive/models.py
+++ library/archive/models.py
@@ -6,24 +6,35 @@ User = get_user_model()
 
 
 class Author(models.Model):
-    ...
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='authors/', default='authors/default_autor.webp', null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Category(models.Model):
     title = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.title
 
 
 class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     title = models.CharField(verbose_name="Назва книги", max_length=250)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books', null=True, blank=True)
+    cover = models.ImageField(upload_to='books/', default='books/default_book_cover.png', null=True, blank=True)
+
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
 
 class Rating(models.Model):
-    post = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.PositiveIntegerField(
         validators=[
@@ -31,13 +42,12 @@ class Rating(models.Model):
             MaxValueValidator(5)   
         ]
     )
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     
 class Comments(models.Model):
-    post = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
